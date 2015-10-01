ab13306d5de5da5783c3ee0b544d859f935bc2f6
Vo Minh Phuong <phuong.vm@pinetech.vn>
2015-10-01 14:05:11 +0700

    update formate wap inpage banner

--- resources/views/delivery/inpage.blade.php
+++ resources/views/delivery/inpage.blade.php
@@ -25,51 +25,67 @@ function showYoMediaPopupAd_{!! $data['zid'] !!}(s) {
     var clickTag = encodeURIComponent("{!! URL::to('/') !!}/track?evt=click&aid={!! $data['aid'] !!}&fpid={!! $data['fpid'] !!}&zid={!! $data['zid'] !!}&rt=1&to={!! urlencode($data['ad']->destination_url) !!}&cs={!! $data['checksum'] !!}");
    
     var a_{!! $data['zid'] !!} = document.getElementById("YomediaInpage_{!! $data['zid'] !!}");
+    var container_width = {!! $width !!};
     if(a_{!! $data['zid'] !!} == null) {
-        @if($eidtype == '#')
-        var content_{!! $data['zid'] !!} = document.getElementById('{!! $eid !!}');
-        @else
-        var content_{!! $data['zid'] !!} = document.getElementsByClassName('{!! $eid !!}')[0];
-        @endif
-        e_{!! $data['zid'] !!} = content_{!! $data['zid'] !!}.childNodes;
-        var p_{!! $data['zid'] !!} = 0;
-        for(var i_{!! $data['zid'] !!}=0; i_{!! $data['zid'] !!} < e_{!! $data['zid'] !!}.length; i_{!! $data['zid'] !!}++) {
-            if(e_{!! $data['zid'] !!}[i_{!! $data['zid'] !!}].clientHeight > 0){
-                p_{!! $data['zid'] !!} = p_{!! $data['zid'] !!}+ e_{!! $data['zid'] !!}[i_{!! $data['zid'] !!}].clientHeight;
-            }
-            
-            if(p_{!! $data['zid'] !!} >= (content_{!! $data['zid'] !!}.clientHeight / 2)){
-                if(typeof(content_{!! $data['zid'] !!}.childNodes[i_{!! $data['zid'] !!} +1]) != 'undefined'){
-                    var eleToInsert = i_{!! $data['zid'] !!} +1;
+    	@if ($eid)
+            @if($eidtype == '#')
+            var content_{!! $data['zid'] !!} = document.getElementById('{!! $eid !!}');
+            @else
+            var content_{!! $data['zid'] !!} = document.getElementsByClassName('{!! $eid !!}')[0];
+            @endif
+            if (typeof content_{!! $data['zid'] !!} != 'undefined' && content_{!! $data['zid'] !!} != null) {
+                e_{!! $data['zid'] !!} = content_{!! $data['zid'] !!}.childNodes;
+                var p_{!! $data['zid'] !!} = 0;
+                for(var i_{!! $data['zid'] !!}=0; i_{!! $data['zid'] !!} < e_{!! $data['zid'] !!}.length; i_{!! $data['zid'] !!}++) {
+                    if(e_{!! $data['zid'] !!}[i_{!! $data['zid'] !!}].clientHeight > 0){
+                        p_{!! $data['zid'] !!} = p_{!! $data['zid'] !!}+ e_{!! $data['zid'] !!}[i_{!! $data['zid'] !!}].clientHeight;
+                    }
+                    
+                    if(p_{!! $data['zid'] !!} >= (content_{!! $data['zid'] !!}.clientHeight / 2)){
+                        if(typeof(content_{!! $data['zid'] !!}.childNodes[i_{!! $data['zid'] !!} +1]) != 'undefined'){
+                            var eleToInsert = i_{!! $data['zid'] !!} +1;
+                        }
+                        else{
+                            var eleToInsert = i_{!! $data['zid'] !!};
+                        }
+        
+                        a_{!! $data['zid'] !!} = domManipulate.create('div', 'YomediaInpage_{!! $data['zid'] !!}', '', ''), content_{!! $data['zid'] !!}.insertBefore(a_{!! $data['zid'] !!}, content_{!! $data['zid'] !!}.childNodes[eleToInsert]);
+                        break;
+                    }
                 }
-                else{
-                    var eleToInsert = i_{!! $data['zid'] !!};
-                }
-
-                a_{!! $data['zid'] !!} = domManipulate.create('div', 'YomediaInpage_{!! $data['zid'] !!}', '', ''), content_{!! $data['zid'] !!}.insertBefore(a_{!! $data['zid'] !!}, content_{!! $data['zid'] !!}.childNodes[eleToInsert]);
-                break;
+                container_width = content_{!! $data['zid'] !!}.clientWidth;
+            }else {
+            	a_{!! $data['zid'] !!} = domManipulate.create('div', 'YomediaInpage_{!! $data['zid'] !!}');
+        		domManipulate.append(a_{!! $data['zid'] !!});
             }
+        @else {
+        	a_{!! $data['zid'] !!} = domManipulate.create('div', 'YomediaInpage_{!! $data['zid'] !!}');
+        	domManipulate.append(a_{!! $data['zid'] !!});
         }
+        @endif
     }
 
     if(a_{!! $data['zid'] !!} == null){
         return false;
     }
     
-    var container_width = content_{!! $data['zid'] !!}.clientWidth;
+	var swdWidth =  wdWidth();
+    var swdHeight =  wdHeight();
+      
+    //var container_width = content_{!! $data['zid'] !!}.clientWidth;
     a_{!! $data['zid'] !!}.style.opacity = 1;
     a_{!! $data['zid'] !!}.style.position = 'relative';
     a_{!! $data['zid'] !!}.style.overflow = 'hidden';
     a_{!! $data['zid'] !!}.style.zIndex  = '0';
-    a_{!! $data['zid'] !!}.style.width = screen.width + "px";
+    a_{!! $data['zid'] !!}.style.width = swdWidth + "px";
     a_{!! $data['zid'] !!}.style.display = 'block';
     a_{!! $data['zid'] !!}.style.visibility = 'visible';
-    a_{!! $data['zid'] !!}.style.height =  (container_width / {!! $width !!} * {!! $height !!} + 40) +'px';
+    a_{!! $data['zid'] !!}.style.height =  swdHeight +'px';
     a_{!! $data['zid'] !!}.style.background = 'transparent';
     var rs = '';
     if(avlInteractModule.isMobile() == true){
-    rs = '<div id="YomediaInpageContent_{!! $data['zid'] !!}" style="display: block; opacity: 1; overflow: hidden; margin: 0px auto; position: fixed; z-index: 1; bottom: 20%; left:0;right:0;max-width: 100%;background: transparent;"><a style="display: block;" href="'+decodeURIComponent(clickTag)+'" target="_blank" id="yomedia-destination-{!! $data['zid'] !!}"><img id="yomedia-inpage-banner" style="width:100%; max-width: 100%;" src="{!! $source !!}"></a></div>';
-    rs += '<div id="more-view_{!! $data['zid'] !!}" style="opacity: 1; float: right; z-index: 3; clear: both; position: absolute; bottom: 0px; left: 0px; width: 100%; text-align: center; background: transparent;height: 30px;"><a href="#" target="_blank" style="color:#FFF;font-size: 1.3em;background: #CCC;padding: 5px 13px;border-radius: 10px;margin: 5px;height: 18px;">Đọc tiếp</a></div>';
+    rs = '<div id="YomediaInpageContent_{!! $data['zid'] !!}" style="height:'+swdHeight+'px;display: block; opacity: 1; overflow: hidden; margin: 0px auto; position: fixed; z-index: 1; bottom: 0px; left:0;right:0;max-width: 100%;background: transparent;"><a style="display: block;" href="'+decodeURIComponent(clickTag)+'" target="_blank" id="yomedia-destination-{!! $data['zid'] !!}"><img id="yomedia-inpage-banner" style="max-width: 100%;" src="{!! $source !!}"></a></div>';
+    rs += '<div id="more-view_{!! $data['zid'] !!}" style="opacity: 1; float: right; z-index: 3; clear: both; position: absolute; bottom: 0px; left: 0px; width: 100%; text-align: center; background: transparent;height: 30px;"><a style="color:#FFF;font-size: 1.3em;background: #CCC;padding: 5px 13px;border-radius: 10px;margin: 5px;height: 18px;">Đọc tiếp</a></div>';
     domManipulate.getElid('YomediaInpage_{!! $data['zid'] !!}').innerHTML = rs;
     domManipulate.getElid('YomediaInpage_{!! $data['zid'] !!}').style.display = 'block';
     
@@ -181,4 +197,39 @@ document.onreadystatechange = function () {
 
     }
 }
+
+function wdWidth(){
+    var myWidth;
+    if( typeof( window.innerWidth ) == 'number' ) {
+        //Non-IE
+        myWidth = window.innerWidth;
+
+    } else if( document.documentElement && ( document.documentElement.clientWidth  ) ) {
+        //IE 6+ in 'standards compliant mode'
+        myWidth = document.documentElement.clientWidth;
+
+    } else if( document.body && ( document.body.clientWidth  ) ) {
+        //IE 4 compatible
+        myWidth = document.body.clientWidth;
+
+    }
+    return myWidth;
+}
+
+function wdHeight(){
+    var myHeight;
+
+    if( typeof( window.innerHeight ) == 'number' ) {
+        //Non-IE
+        myHeight = window.innerHeight;
+    } else if( document.documentElement && (  document.documentElement.clientHeight ) ) {
+        //IE 6+ in 'standards compliant mode'
+        myHeight = document.documentElement.clientHeight;
+    } else if( document.body && (  document.body.clientHeight ) ) {
+        //IE 4 compatible
+        myHeight = document.body.clientHeight;
+    }
+
+    return myHeight;
+};
 @include("footer")
