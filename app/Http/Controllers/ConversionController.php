27a438be702e8ce6db9f39c29a2f770ef64e4c54
Vo Minh Phuong <phuong.vm@pinetech.vn>
2015-10-20 18:44:36 +0700

    Luu them website id va banner id trong conversion

--- app/Http/Controllers/ConversionController.php
+++ app/Http/Controllers/ConversionController.php
@@ -1,6 +1,7 @@
 <?php namespace App\Http\Controllers;
 use App\Models\Conversion;
 use App\Models\RawTrackingConversion;
+use Cookie;
 
 class ConversionController extends Controller
 {
@@ -21,8 +22,20 @@ class ConversionController extends Controller
             $objConversion = $conversionModel->getConversion($conversionID);
             if ($objConversion) {
                 $param = urldecode($param);
+                $cookieKey = "Conv_{$objConversion->campaign_id}";
+                $infoConversion = Cookie::get($cookieKey);
+                
+                if (!empty($infoConversion)) {
+                    $arrInfoConversion = json_decode($infoConversion);
+                    $arrParam = json_decode($param);
+                    $arrParam->wid = $arrInfoConversion->wid;
+                    $arrParam->bid = $arrInfoConversion->bid;
+                    $param = json_encode($arrParam);
+                }
+                
                 if ($rawTrackingConversionModel->addConversion($conversionID, $objConversion->campaign_id, $param)) {
                     $responseType = Conversion::RESPONSE_TYPE_CONVERSION_SUCCESS;
+                    return response('')->withCookie(cookie($cookieKey));
                 } else {
                     $responseType = Conversion::RESPONSE_TYPE_CONVERSION_ERROR;
                 }
