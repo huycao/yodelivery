e30ab524fd4bf0d4ce6ac94edf8e796d9e11f83e
Phuong Vo <phuong.vm@pinetech.vn>
2015-12-03 13:10:24 +0700

    Update audience

--- app/Http/Controllers/DeliveryController.php
+++ app/Http/Controllers/DeliveryController.php
@@ -81,6 +81,7 @@ class DeliveryController extends Controller
 		if ($showBanner !== FALSE) {
 		    $flightWebsiteID = $showBanner;
 		}
+		//$uuid = $trackingModel->getVisitorId();
 
 		//ghi log trước khi xử lý
 		//$logPreProcess = $trackingModel->logPreProcess($requestType, $data);
@@ -108,13 +109,14 @@ class DeliveryController extends Controller
 							//read redis 1
 							$flightWebsites = $deliveryModel->getAvailableAds($adZone->publisher_site_id, $adZone->ad_format_id, $flightWebsiteID, $platform);
 							pr($flightWebsites);
-							if($flightWebsites){
+							if($flightWebsites){								
 								//sort available flights base on priority and retargeting
 								//TO DO retargeting
 								$flightWebsites = $deliveryModel->sortAvailableFlightWebsites($flightWebsites);
 								//lấy ad từ list thỏa điều kiện để trả về
 								$deliveryInfo = $deliveryModel->getFullFlightInfo($flightWebsites, $adZone->publisher_site_id, $adZone->ad_format_id);
 								pr($deliveryInfo);
+								$redis = new RedisBaseModel(env('REDIS_HOST', '127.0.0.1'), env('REDIS_PORT_6', '6379'), false);
 								foreach ($flightWebsites as $k => $flightWebsite) {
 									if(!empty($flightWebsite) && !empty($deliveryInfo['flightDates'][$flightWebsite->flight_id]) && !empty($deliveryInfo['flights'][$flightWebsite->flight_id])){
 
@@ -140,8 +142,10 @@ class DeliveryController extends Controller
 										        		$check = false;
 									        			$audience = json_decode($flight->audience, true);
 									        			if (!empty ($audience['audience_id'])) {
-								        					if (!empty($_COOKIE["yoAu_{$audience['audience_id']}"])) {
-									        					$check = true;
+								        					if (isset($_COOKIE["yoAu_{$audience['audience_id']}"]) && !empty($_COOKIE["uuid"])) {
+								        						if ($_COOKIE["yoAu_{$audience['audience_id']}"] == '1'){
+								        							$check = true;						        					
+								        						}								        						
 									        				}
 
 									        				if ($audience['operator'] === 'not in') {
@@ -393,25 +397,29 @@ class DeliveryController extends Controller
 				}
 				$rawTrackingSummary->addSummary($event, $flightWebsite->website_id, $adZone->id, $adZone->ad_format_id, $flightWebsite->flight_id, $flightWebsite->id, $flightWebsite->flight->ad_id, $flightWebsite->flight->campaign_id, $flightWebsite->publisher_base_cost, $isOverReport);
 
-				$uuid = $trackingModel->getVisitorId();
-				if ('impression' === $event) {
-					//Collection data
+				$uuid = $trackingModel->getVisitorId();				
+				
+		        if ('impression' === $event || 'click' === $event) {
+		        	//Collection data		        	
 		        	if (!empty($flightWebsite->ad->audience_id)) {
 		        		$audience_id = $flightWebsite->ad->audience_id;
-		        		if (empty($_COOKIE["yoAu_{$audience_id}"])) {
-			        		setcookie("yoAu_{$audience_id}", 1, time()+(86400*365), '/', getWebDomain(AD_SERVER_FILE));
-
-			        	}
-	        			$redis = new RedisBaseModel(env('REDIS_HOST', '127.0.0.1'), env('REDIS_PORT_6', '6379'), false);
-	        			$redis->pfadd("au.$audience_id", array($uuid));
+		        		if (!isset($_COOKIE["yoAu_{$audience_id}"]) || $_COOKIE["yoAu_{$audience_id}"] == "0") {		        			
+			        		setcookie("yoAu_{$audience_id}", "1", time()+(86400*365), '/', getWebDomain(DOMAIN_COOKIE));
+		        			$redis = new RedisBaseModel(env('REDIS_HOST', '127.0.0.1'), env('REDIS_PORT_6', '6379'), false);
+	        				$redis->pfadd("au.$audience_id", array($uuid));	        				
+			        	}			        	
+			        	$rawTrackingAudience= new RawTrackingAudience();
+		        		$rawTrackingAudience->addAudience($uuid, $audience_id, $flightWebsite->ad->id, $event);
 		        	}
-		        }
 
-		        //Tracking audience
-		        if ('impression' === $event || 'click' === $event) {
-		        	if (!empty($flightWebsite->flight->audience) || !empty($flightWebsite->ad->audience_id)) {
-		        		//$rawTrackingAudience= new RawTrackingAudience();
-		        		//$rawTrackingAudience->addAudience($uuid, $flightWebsite->ad->id, $event);
+		        	//Tracking audience
+		        	if (!empty($flightWebsite->flight->audience)) {
+		        		$audience = json_decode($flightWebsite->flight->audience, true);
+		        		if ($audience['operator'] == 'not in'){		        			
+		        			setcookie("yoAu_{$audience['audience_id']}", "0", time()+(86400*365), '/', getWebDomain(DOMAIN_COOKIE));	
+		        		}
+		        		$rawTrackingAudience= new RawTrackingAudience();
+		        		$rawTrackingAudience->addAudience($uuid, $audience['audience_id'], $flightWebsite->ad->id, $event);
 		        	}
 		        }
 
--- app/Models/RawTrackingAudience.php
+++ app/Models/RawTrackingAudience.php
@@ -1,41 +1,38 @@
 <?php namespace App\Models;
-use Jenssegers\Mongodb\Model as Moloquent;
 
-class RawTrackingAudience extends Moloquent {
+class RawTrackingAudience{
 
     protected $table      = 'tracking_audience';
     protected $connection = 'mongodb';
     public $timestamps    = false;
-
-
-    public function addAudience($uuid, $bid, $event) {
-        $data = [
-            'uuid' => $uuid,
-            'bid'  => intval($bid),
-            'time' => intval(time())
+   
+    public function addAudience($uuid, $audience_id, $bid, $event){
+        $info = [
+            '_id' => $uuid
         ];
 
-        return $this->incrementUpsert($event, 1, $data, $data);
+        $where = [
+            '_id'   =>  $uuid
+        ];
+        $table = "{$this->table}_{$bid}_{$audience_id}";
+
+        return $this->incrementUpsert($table, $event, 1, $where, $info);
     }
 
-    public function incrementUpsert($column, $amount = 1, array $where = array(), array $extra = array())
+    /**
+     * increment record's column value if exists or insert new record.
+     *
+     * @param  array  $values
+     * @param  array  $options
+     * @return int
+     */
+    public function incrementUpsert($table, $column, $amount = 1, array $where = array(), array $extra = array())
     {   
-        $db = \DB::connection('mongodb')->collection($this->table);
-        $db1 = \DB::connection('mongodb')->collection($this->table);
-        $tmp = $extra;
-        if (isset($tmp['time'])) {
-            unset($tmp['time']);
-        }
-        unset($where['time']);
-        $a = $db1->where($tmp)->where('time', 'exists', true)->get();        
-        if (!empty($a)) {
-            unset($extra['time']);
-        }   
-
+        $db = \DB::connection('mongodb')->collection($table);
         $query = array('$inc' => array($column => $amount));
 
         if ( ! empty($extra))
-        {            
+        {
             $query['$set'] = $extra;
         }
        
@@ -53,5 +50,6 @@ class RawTrackingAudience extends Moloquent {
         }
 
         return $db->update($query, array('upsert' => true, 'multiple' => 0));
+
     }
 }
