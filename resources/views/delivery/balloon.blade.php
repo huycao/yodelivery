bbaaab206ec5789a73af6e6c1ffc3d01102805e2
Phuong Vo <phuong.vm@pinetech.vn>
2016-02-01 17:04:03 +0700

    Fix bug open landing page in safari

--- resources/views/delivery/balloon.blade.php
+++ resources/views/delivery/balloon.blade.php
@@ -194,7 +194,16 @@ function clickTrackingYomedia_{!! $data['zid'] !!}() {
     if (typeof _YoClick != 'undefined' && avlHelperModule.validateUrl(_YoClick)) {
     	avlHelperModule.embedTracking(_YoClick);
     }
-	window.open(clickTag);
+	var opened = window.open(clickTag, '_blank');
+    if (typeof opened == 'undefined') {
+        tagLink = document.createElement('a');       
+        tagLink.setAttribute('id', 'yomedia-tag-link-{!! $data['zid'] !!}');
+        tagLink.setAttribute('href', clickTag);
+        tagLink.setAttribute('target', '_blank');
+        tagLink.style.display = 'none';
+        document.body.appendChild(tagLink);
+        fireEventYomedia_{!! $data['zid'] !!}(document.getElementById("yomedia-tag-link-{!! $data['zid'] !!}"), "click");
+    }
 }
 
 <?php
@@ -231,6 +240,58 @@ function iFrameListener_{!! $data['zid'] !!}(event){
      if ('string' == typeof fn_{!! $data['zid'] !!} && fn_{!! $data['zid'] !!}.toLowerCase().indexOf("yomedia") >= 0) {
     	 eval(fn_{!! $data['zid'] !!});
      }
+}
+
+function fireEventYomedia_{!! $data['zid'] !!}(node, eventName) {
+    // Make sure we use the ownerDocument from the provided node to avoid cross-window problems
+    var doc;
+    if (node.ownerDocument) {
+        doc = node.ownerDocument;
+    } else if (node.nodeType == 9){
+        // the node may be the document itself, nodeType 9 = DOCUMENT_NODE
+        doc = node;
+    } else {
+        throw new Error("Invalid node passed to fireEvent: " + node.id);
+    }
+
+     if (node.dispatchEvent) {
+        // Gecko-style approach (now the standard) takes more work
+        var eventClass = "";
+
+        // Different events have different event classes.
+        // If this switch statement can't map an eventName to an eventClass,
+        // the event firing is going to fail.
+        switch (eventName) {
+            case "click": // Dispatching of 'click' appears to not work correctly in Safari. Use 'mousedown' or 'mouseup' instead.
+            case "mousedown":
+            case "mouseup":
+                eventClass = "MouseEvents";
+                break;
+
+            case "focus":
+            case "change":
+            case "blur":
+            case "select":
+                eventClass = "HTMLEvents";
+                break;
+
+            default:
+                throw "fireEvent: Couldn't find an event class for event '" + eventName + "'.";
+                break;
+        }
+        var event = doc.createEvent(eventClass);
+
+        var bubbles = eventName == "change" ? false : true;
+        event.initEvent(eventName, bubbles, true); // All events created as bubbling and cancelable.
+
+        event.synthetic = true; // allow detection of synthetic events
+        node.dispatchEvent(event, true);
+    } else  if (node.fireEvent) {
+        // IE-old school style
+        var event = doc.createEventObject();
+        event.synthetic = true; // allow detection of synthetic events
+        node.fireEvent("on" + eventName, event);
+    }
 }
 
 var fn_{!! $data['zid'] !!}='';
